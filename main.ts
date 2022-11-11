54a54e72a7e59b11b7d8247923808d4fcfdbb538
Turbidsoul <sccn.sq@gmail.com>
2022-11-11 17:37:49 +0800

    feat: (csv) 增加head
    默认首行为head
    
    在第一行加上 %{"head": false}% 可以配置

--- main.ts
+++ main.ts
@@ -7,31 +7,17 @@ export default class SvgbobBlocksPlugin extends Plugin {
 		this.registerMarkdownCodeBlockProcessor(
 			"svgbob",
 			async (src, el, ctx) => {
-				await this.processor("svgbob", src, el, ctx);
+				await this.svgbobProcessor(src, el, ctx);
 			}
 		);
 		this.registerMarkdownCodeBlockProcessor("csv", async (src, el, ctx) => {
-			const rows = src.split("\n").filter((row) => row.length > 0);
-
-			const table = el.createEl("table");
-			const body = table.createEl("tbody");
-
-			for (let i = 0; i < rows.length; i++) {
-				const cols = rows[i].split(",");
-
-				const row = body.createEl("tr");
-
-				for (let j = 0; j < cols.length; j++) {
-					row.createEl("td", { text: cols[j] });
-				}
-			}
+			await this.csvProcessor(src, el, ctx);
 		});
 	}
 
 	onunload() {}
 
-	async processor(
-		type: string,
+	async svgbobProcessor(
 		src: string,
 		el: HTMLElement,
 		ctx?: MarkdownPostProcessorContext
@@ -47,5 +33,43 @@ export default class SvgbobBlocksPlugin extends Plugin {
 				}
 			}
 		);
+	}
+
+	async csvProcessor(
+		src: string,
+		el: HTMLElement,
+		ctx?: MarkdownPostProcessorContext
+	) {
+		let rows = src.split("\n").filter((row) => row.length > 0);
+		let head = true;
+		try {
+			const matches = /^%\s(.*)\s%$/[Symbol.match](rows[0].trim());
+			if (matches) {
+				rows.splice(0, 1);
+				const cfg = JSON.parse(matches[1]);
+				head = cfg.head ?? true;
+			}
+		} catch (e) {
+			el.createDiv("错误");
+			return;
+		}
+		const table = el.createEl("table");
+		if (head) {
+			const thead = table.createEl("thead");
+			const heads = rows.splice(0, 1)[0].split(",");
+			for (let i in heads) {
+				thead.createEl("th", { text: heads[i] });
+			}
+		}
+		const body = table.createEl("tbody");
+		for (let i = 0; i < rows.length; i++) {
+			const cols = rows[i].split(",");
+
+			const row = body.createEl("tr");
+
+			for (let j = 0; j < cols.length; j++) {
+				row.createEl("td", { text: cols[j] });
+			}
+		}
 	}
 }
